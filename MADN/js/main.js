6c08127b8768727305aa66dfbf1cb5980f0eb988
Max <42644616+byMax2@users.noreply.github.com>
2019-09-27 09:42:09 +0200

    Add files via upload

--- MADN/js/main.js
+++ MADN/js/main.js
@@ -1,10 +1,18 @@
 var fields = [7, 10, 13, 16, 23, 24, 25, 26, 27, 38, 49, 48, 47, 46, 45, 52, 55, 58, 61, 60, 59, 56, 53, 50, 43, 42, 41, 40, 39, 28, 17, 18, 19, 20, 21, 14, 11, 8, 5, 6];
+var fieldsgruen = [7, 10, 13, 16, 23, 24, 25, 26, 27, 38, 49, 48, 47, 46, 45, 52, 55, 58, 61, 60, 59, 56, 53, 50, 43, 42, 41, 40, 39, 28, 17, 18, 19, 20, 21, 14, 11, 8, 5, 6, 9, 12, 15, 22];
+var fieldsrot = [49, 48, 47, 46, 45, 52, 55, 58, 61, 60, 59, 56, 53, 50, 43, 42, 41, 40, 39, 28, 17, 18, 19, 20, 21, 14, 11, 8, 5, 6, 7, 10, 13, 16, 23, 24, 25, 26, 27, 38, 37, 36, 35, 34];
+var fieldsgelb = [17, 18, 19, 20, 21, 14, 11, 8, 5, 6, 7, 10, 13, 16, 23, 24, 25, 26, 27, 38, 49, 48, 47, 46, 45, 52, 55, 58, 61, 60, 59, 56, 53, 50, 43, 42, 41, 40, 39, 28, 29, 30, 31, 32];
+var fieldsblau = [59, 56, 53, 50, 43, 42, 41, 40, 39, 28, 17, 18, 19, 20, 21, 14, 11, 8, 5, 6, 7, 10, 13, 16, 23, 24, 25, 26, 27, 38, 49, 48, 47, 46, 45, 52, 55, 58, 61, 60, 57, 54, 51, 44];
 var current = "gruen";
 var currentamount = 0;
 var start = [["gelb1", "gelb2", "gelb3", "gelb4"], ["gruen1", "gruen2", "gruen3", "gruen4"], ["blau1", "blau2", "blau3", "blau4"], ["rot1", "rot2", "rot3", "rot4"]];
 var hausfelder = [[29, 30, 31, 32], [9, 12, 15, 22], [44, 51, 54, 57], [34, 35, 36, 37]];
 var figuren = [["gelb1", "gelb2", "gelb3", "gelb4"], ["gruen1", "gruen2", "gruen3", "gruen4"], ["blau1", "blau2", "blau3", "blau4"], ["rot1", "rot2", "rot3", "rot4"]];
 var selection = null;
+var dice = 0;
+var actionneed = false;
+var endgame = false;
+var player = 4;
 
 $(document).ready(function() {
 	getRightsize();
@@ -22,29 +30,29 @@ $(document).ready(function() {
 	$('#unten').html(html);
 	
 	$('#feld7').css('background-color', "green");
-	$('#feld9').css('background-color', "green");
-	$('#feld12').css('background-color', "green");
-	$('#feld15').css('background-color', "green");
-	$('#feld22').css('background-color', "green");
+	$('#feld9').css('background-color', "green").addClass("zielgruen");
+	$('#feld12').css('background-color', "green").addClass("zielgruen");
+	$('#feld15').css('background-color', "green").addClass("zielgruen");
+	$('#feld22').css('background-color', "green").addClass("zielgruen");
 
 	$('#feld17').css('background-color', "yellow");
-	$('#feld29').css('background-color', "yellow");
-	$('#feld30').css('background-color', "yellow");
-	$('#feld31').css('background-color', "yellow");
-	$('#feld32').css('background-color', "yellow");
+	$('#feld29').css('background-color', "yellow").addClass("zielgelb");
+	$('#feld30').css('background-color', "yellow").addClass("zielgelb");
+	$('#feld31').css('background-color', "yellow").addClass("zielgelb");
+	$('#feld32').css('background-color', "yellow").addClass("zielgelb");
 
-	$('#feld33').html("<p><b>Würfel</b></p>");
+	$('#feld33').html("<p><b>Würfel</b><br>(D)</p>");
 
-	$('#feld34').css('background-color', "red");
-	$('#feld35').css('background-color', "red");
-	$('#feld36').css('background-color', "red");
-	$('#feld37').css('background-color', "red");
+	$('#feld34').css('background-color', "red").addClass("zielrot");
+	$('#feld35').css('background-color', "red").addClass("zielrot");
+	$('#feld36').css('background-color', "red").addClass("zielrot");
+	$('#feld37').css('background-color', "red").addClass("zielrot");
 	$('#feld49').css('background-color', "red");
 
-	$('#feld44').css('background-color', "#2E2EFE");
-	$('#feld51').css('background-color', "#2E2EFE");
-	$('#feld54').css('background-color', "#2E2EFE");
-	$('#feld57').css('background-color', "#2E2EFE");
+	$('#feld44').css('background-color', "#2E2EFE").addClass("zielblau");
+	$('#feld51').css('background-color', "#2E2EFE").addClass("zielblau");
+	$('#feld54').css('background-color', "#2E2EFE").addClass("zielblau");
+	$('#feld57').css('background-color', "#2E2EFE").addClass("zielblau");
 	$('#feld59').css('background-color', "#2E2EFE");
 	$('.hausfeld').each(function() {
 		var id = $(this).prop('id');
@@ -65,22 +73,9 @@ $(document).ready(function() {
 	});
 	$('h1').html("Mensch &Auml;rgere Dich <b>Nicht</b>");
 	$('#current').html("Der Spieler mit der Farbe <b>" + current.toUpperCase() + "</b> startet das Spiel!");
-	$('#feld33').on('click', rollDice);
-	$('.spielfeldfeld').on('click', function() {
-		if ( $(this).children().length > 0 ) {
-			if ($(this).children().eq(0).hasClass("spielfigur")) {
-				if ($(this).children().eq(0).prop('id').includes(current)) {
-					if (isOnlyOneOut() == null) {
-						clearSelection();
-						selection = $(this).children().eq(0).prop('id');
-						$('#' + selection).css("border-width", "2px");
-					} else {
-						alert("Du kannst nur Figuren auswaehlen, wenn du mehrere auf dem Feld hast!");
-					}
-				}
-			}
-		}
-	});
+	//$('#feld33').css('background-color', 'green');
+	$('#feld33').on('click', rollNewDice);
+	$('.feld').on('click', selectfigur);
 });
 
 $(window).resize(function() {
@@ -89,87 +84,112 @@ $(window).resize(function() {
 
 $(window).on("keypress", function(e) {
 	if (e.which == 100)
-		rollDice();
+		rollNewDice();
 });
 
-function rollDice() {
-var dice =  getRndZahl();
+function selectfigur() {
+	if (actionneed) 
+		if ( $(this).children().length > 0) {
+			var child = $(this).children().eq(0);
+			if (child.hasClass("spielfigur") && !($(this).hasClass("zielgruen") || $(this).hasClass("zielgelb") || $(this).hasClass("zielblau") || $(this).hasClass("zielrot"))) 
+				if (child.prop('id').includes(current)) {
+						clearSelection();
+						selection = child.prop('id');
+						var figurarray = getFigurenArray();
+						var figur = figuren[figurarray[0]][figurarray[1]];
+						if (needsASix()) { // AUS DEM HAUS RAUSGEHEN
+							var figurdiv = $('#' + figur).html();
+							$('#' + figur).html("");
+							var newfield = 'feld' + fields[getFirstField()];
+							if ($('#' + newfield).html() != "")
+								kickFigur(newfield);
+								$('#' + newfield).html(figurdiv);
+							figuren[figurarray[0]][figurarray[1]] = newfield;
+							clearSelection();
+							$('#info').html("Diese Figur ist nun auf deinem Startfeld, würfle nochmal!")
+							actionneed = false;
+						} else {
+							if (child.parent().hasClass("hausfeld")) {
+								if ($('#feld' + fields[getFirstField()]).html() != null && $('#feld' + fields[getFirstField()]).html().includes("figur" + current)) {
+									$('#info').html("Du kannst deine eigenen Figuren nicht rausschmeißen, wähle eine andere Figur aus!");
+								} else {
+									var figurdiv = $('#' + figur).html();
+									$('#' + figur).html("");
+									var newfield = 'feld' + fields[getFirstField()];
+									if ($('#' + newfield).html() != "")
+										kickFigur(newfield);
+										$('#' + newfield).html(figurdiv);
+									figuren[figurarray[0]][figurarray[1]] = newfield;
+									clearSelection();
+										$('#info').html("Diese Figur ist nun auf deinem Startfeld, würfle nochmal!");
+									actionneed = false;
+								}
+							} else {
+
+								if ($('#' + getNextField(figur, dice)).html() != null && $('#' + getNextField(figur, dice)).html().includes("figur" + current)) {
+									if (canDoSomething()) {
+										$('#info').html("Du kannst deine eigenen Figuren nicht rausschmeißen, wähle eine andere Figur aus!");
+									} else {
+										clearSelection();
+										actionneed = false;
+										currentamount = 0;
+										current = getNextPlayer();
+										$('#current').html("Der Spieler mit der Farbe <b>" + current.toUpperCase() + "</b> ist nun am Zug!");
+										$('#info').html("Weil du mit dieser Zahl nichts machen kannst, ist nun " + current.toUpperCase() + " am Zug!")
+									}
+								} else {
+									var figurdiv = $('#' + figur).html();
+									$('#' + figur).html("");
+									var newfield = getNextField(figur, dice);
+									if ($('#' + newfield).html() != "")
+										kickFigur(newfield);
+									$('#' + newfield).html(figurdiv);
+									figuren[figurarray[0]][figurarray[1]] = newfield;
+									clearSelection();
+									actionneed = false;
+									checkWin();
+									if (!endgame)
+										if (dice == 6) 
+											$('#info').html("Da du eine Sechs gewürfet hast, darfst du nocheinmal würfeln!");
+										else {
+											currentamount = 0;
+											current = getNextPlayer();
+											$('#current').html("Der Spieler mit der Farbe <b>" + current.toUpperCase() + "</b> ist nun am Zug!");
+											$('#info').html("Weil ist nun der Zug abgeschlossen wurde ist nun " + current.toUpperCase() + " am Zug!")
+										}
+								}
+							}
+
+						}
+					}
+		}
+}
+
+function rollNewDice() {
+	if (!actionneed && !endgame) {
+		dice = getRndZahl();
 		$('#dice').html("Es wurde eine <b>" + dice + "</b> Gewürfelt!");
-		if (needsASix()) {
+		if (needsASix()) 
 			if (dice == 6) {
-				var figurarray = getNextHomeFigur();
-				var figur = figuren[figurarray[0]][figurarray[1]];
-				var figurdiv = $('#' + figur).html();
-				$('#' + figur).html("");
-				var newfield = 'feld' + fields[getFirstField()];
-				if ($('#' + newfield).html() != "")
-					kickFigur(newfield);
-					$('#' + newfield).html(figurdiv);
-				figuren[figurarray[0]][figurarray[1]] = newfield;
-			} else {
+				actionneed = true;
+				$('#info').html("Wähle jetzt die Figur aus, mit der du aufs Spielbrett gehen willst!");
+			} else 
 				if (currentamount <= 1)
 					currentamount++;
 				else {
 					currentamount = 0;
 					current = getNextPlayer();
-					$('#current').html("Der Spieler mit der Farbe <b>" + current.toUpperCase() + "</b> ist nun am Zug!");
-						clearSelection();
-				}
-			}
-		} else {
-			if (dice == 6) {
-				if ($('#feld' + fields[getFirstField()]).html() == "") {
-					if (getNextHomeFigur() != null) {
-						var figurarray = getNextHomeFigur();
-						var figur = figuren[figurarray[0]][figurarray[1]];
-						var figurdiv = $('#' + figur).html();
-						$('#' + figur).html("");
-						var newfield = 'feld' + fields[getFirstField()];
-						if ($('#' + newfield).html() != "")
-							kickFigur(newfield);
-						$('#' + newfield).html(figurdiv);
-						figuren[figurarray[0]][figurarray[1]] = newfield;
-						clearSelection();
-						selection = $('#' + newfield).children().eq(0).prop('id');
-						$('#' + selection).css("border-width", "2px");
-						return;
-					}
-				}
-			}
-			var figurarray = isOnlyOneOut();
-			if (figurarray != null ) {
-				var figur = figuren[figurarray[0]][figurarray[1]];
-				var figurdiv = $('#' + figur).html();
-				$('#' + figur).html("");
-				var newfield = getNextField(figur, dice);
-				if ($('#' + newfield).html() != "")
-					kickFigur(newfield);
-				$('#' + newfield).html(figurdiv);
-				figuren[figurarray[0]][figurarray[1]] = newfield;
-				current = getNextPlayer();
-					$('#current').html("Der Spieler mit der Farbe <b>" + current.toUpperCase() + "</b> ist nun am Zug!");
-				clearSelection();
-			} else {
-				if (selection != null) {
-				var figurarray = getFigurenArray();
-				var figur = figuren[figurarray[0]][figurarray[1]];
-					var figurdiv = $('#' + figur).html();
-					$('#' + figur).html("");
-					var newfield = getNextField(figur, dice);
-					if ($('#' + newfield).html() != "")
-						kickFigur(newfield);
-					$('#' + newfield).html(figurdiv);
-					figuren[figurarray[0]][figurarray[1]] = newfield;
-					current = getNextPlayer();
+					$('#info').html("Es wurde keine Sechs gewürfelt, deswegen ist nun " + current.toUpperCase() + " am Zug!")
 					$('#current').html("Der Spieler mit der Farbe <b>" + current.toUpperCase() + "</b> ist nun am Zug!");
 					clearSelection();
-				} else {
-					alert("Du musst erst noch eine Figur auswaehlen!");
-					return;
-				}
+				
 			}
+		else {
+			actionneed = true;
+			$('#info').html("Wähle jetzt die Figur aus, mit der du weitergehen willst!");
 		}
 	}
+}
 
 function getRightsize() {
 	if ($('#content').width() < $('#content').height()) {
@@ -182,6 +202,58 @@ function getRightsize() {
 }
 
 // LOGIK
+
+function checkWin() {
+	var felder = getRightFieldList();
+	var win = true;
+	for (var i = felder.length-4; i < felder.length; i++) {
+		if ($('#feld' + felder[i]).html() == "") {
+			win = false;
+			return false;
+		}
+	}
+	if (win) {
+		endgame = true;
+		$('#info').html("Der Spieler mit der Farbe <b>" + current.toUpperCase() + "</b> hat das Spiel gewonnen!");
+		$('#current').html("");
+		$('#dice').html("");
+	}
+
+}
+
+function canDoSomething() {
+	var farbe = -1;
+	switch (current) {
+		case "gelb":
+			farbe = 0;
+			break;
+		case "gruen":
+			farbe = 1;
+			break;
+		case "blau":
+			farbe = 2;
+			break;
+		case "rot":
+			farbe = 3;
+			break;
+	}
+	for (var i = 0; i < figuren[farbe].length; i++){
+		var currentfield = figuren[farbe][i];
+		var nxtField = $('#' + getNextField(currentfield, dice));
+		var nxtFieldhtml = nxtField.html();
+		var isInGoal = $('#' + currentfield).hasClass("ziel" + current);
+		if (!isInGoal) {
+			if (nxtFieldhtml == null) {
+				return true;
+			} else {
+				if (!nxtFieldhtml.includes("figur" + current)) {
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
 
 function kickFigur(field) {
 	var id = $('#' + field).children().eq(0).prop('id').replace('figur', '');
@@ -300,33 +372,64 @@ function getNextHomeFigur() {
 function needsASix() {
 	switch(current) {
 			case "gelb":
-				return figuren[0][0] == "gelb1" &&
-				 figuren[0][1] == "gelb2" &&
-				 figuren[0][2] == "gelb3" &&
-				 figuren[0][3] == "gelb4";
+				return (figuren[0][0].includes("gelb1") || $('#' + figuren[0][0]).hasClass("zielgelb")) &&
+				 (figuren[0][1].includes("gelb2") || $('#' + figuren[0][1]).hasClass("zielgelb")) &&
+				 (figuren[0][2].includes("gelb3") || $('#' + figuren[0][2]).hasClass("zielgelb")) &&
+				 (figuren[0][3].includes("gelb4") || $('#' + figuren[0][3]).hasClass("zielgelb"));
 				break;
 			case "gruen":
-				return figuren[1][0] == "gruen1" &&
-				 figuren[1][1] == "gruen2" &&
-				 figuren[1][2] == "gruen3" &&
-				 figuren[1][3] == "gruen4";
+				return (figuren[1][0].includes("gruen1") || $('#' + figuren[1][0]).hasClass("zielgruen")) &&
+				 (figuren[1][1].includes("gruen2") || $('#' + figuren[1][1]).hasClass("zielgruen")) &&
+				 (figuren[1][2].includes("gruen3") || $('#' + figuren[1][2]).hasClass("zielgruen")) &&
+				 (figuren[1][3].includes("gruen4") || $('#' + figuren[1][3]).hasClass("zielgruen"));
 				break;
 			case "blau":
-				return figuren[2][0] == "blau1" &&
-				 figuren[2][1] == "blau2" &&
-				 figuren[2][2] == "blau3" &&
-				 figuren[2][3] == "blau4";
+				return (figuren[2][0].includes("blau1") || $('#' + figuren[2][0]).hasClass("zielblau")) &&
+				 (figuren[2][1].includes("blau2") || $('#' + figuren[2][1]).hasClass("zielblau")) &&
+				 (figuren[2][2].includes("blau3") || $('#' + figuren[2][2]).hasClass("zielblau")) &&
+				 (figuren[2][3].includes("blau4") || $('#' + figuren[2][3]).hasClass("zielblau"));
 				break;
 			case "rot":
-				return figuren[3][0] == "rot1" &&
-				 figuren[3][1] == "rot2" &&
-				 figuren[3][2] == "rot3" &&
-				 figuren[3][3] == "rot4";
+				return (figuren[3][0].includes("rot1") || $('#' + figuren[3][0]).hasClass("zielrot")) &&
+				 (figuren[3][1].includes("rot2") || $('#' + figuren[3][1]).hasClass("zielrot"))  &&
+				 (figuren[3][2].includes("rot3") || $('#' + figuren[3][2]).hasClass("zielrot"))  &&
+				 (figuren[3][3].includes("rot4") || $('#' + figuren[3][3]).hasClass("zielrot"));
 			break;
 		}
 }
 
 function getNextPlayer() {
+	if (player == 2) {
+	switch(current) {
+		case "gelb":
+			return "rot";
+			break;
+		case "gruen":
+			return "rot";
+			break;
+		case "rot":
+			return "gruen";
+			break;
+		case "blau":
+			return "gruen";
+		break;
+		}
+	} else if (player == 3) {
+	switch(current) {
+		case "gelb":
+			return "rot";
+			break;
+		case "gruen":
+			return "rot";
+			break;
+		case "rot":
+			return "blau";
+			break;
+		case "blau":
+			return "gruen";
+		break;
+	}
+	}
 	switch(current) {
 			case "gelb":
 				return "gruen";
@@ -341,25 +444,56 @@ function getNextPlayer() {
 				return "gelb";
 			break;
 		}
-		return current;
+}
+
+function show() {
+	i = 0;
+	var felder = getRightFieldList();
+	var currentfield = "feld" + felder[0];
+	var showinterval = setInterval(function () {
+		if (i > felder.length)
+			clearInterval(showinterval);
+			var nxtField = getNextField(currentfield, 1);
+			$('#' + nxtField).text(i);
+			currentfield = nxtField;
+		i++;
+	}, 50);
+}
+
+
+
+function getRightFieldList() {
+	var felder;
+	switch (current) {
+		case "gruen": 
+			felder = fieldsgruen;
+			break;
+		case "gelb":
+			felder = fieldsgelb;
+			break;
+		case "blau":
+			felder = fieldsblau;
+			break;
+		case "rot":
+			felder = fieldsrot;
+			break;
+	}
+	return felder;
 }
 
 function getNextField(currentfield, dice) {
-	/*switch(current){
-		case "gruen":
-			if (currentfield+dice > )
-			break;
-	}*/
-for (var i = 0; i < fields.length; i++) {
-	if (currentfield == "feld" + fields[i]) {
-		if (i+dice >= fields.length)
-			return "feld" + fields[i + dice - fields.length];
-		else
-		return "feld" + fields[i + dice];
+	var felder = getRightFieldList();
+	
+	for (var i = 0; i < felder.length; i++) {
+		if (currentfield == "feld" + felder[i]) {
+			if (i+dice <= felder.length)
+				return "feld" + felder[i + dice];
+			else
+				return "feld" + felder[felder.length-1];
 		}
 	}
 }
 
 function getRndZahl() {
-	return /*Math.floor((Math.random() * 6) + 1)*/6;
+	return Math.floor((Math.random() * 6) + 1);
 }
