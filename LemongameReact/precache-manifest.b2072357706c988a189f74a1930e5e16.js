08e5a2a7cda3291675ad026f479b7a759638e282
Max <42644616+byMax2@users.noreply.github.com>
2020-10-13 07:57:43 +0200

    Added Lemongame React Code from VSC

---
+++ LemongameReact/precache-manifest.b2072357706c988a189f74a1930e5e16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56b9c55619c18d7ee74fb70aa31b326f",
+    "url": "/LemongameReact/index.html"
+  },
+  {
+    "revision": "38248dd1c5b0f8ed534e",
+    "url": "/LemongameReact/static/css/main.c13dc507.chunk.css"
+  },
+  {
+    "revision": "bc276e908aa7935cfd07",
+    "url": "/LemongameReact/static/js/2.a28b72a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LemongameReact/static/js/2.a28b72a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38248dd1c5b0f8ed534e",
+    "url": "/LemongameReact/static/js/main.047f796a.chunk.js"
+  },
+  {
+    "revision": "c99d978aca42ce260f57",
+    "url": "/LemongameReact/static/js/runtime-main.55898d56.js"
+  }
+]);
