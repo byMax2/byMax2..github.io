0e193d6ce3da0dab9f0e7f2a080ae86d03e3c0c7
Max <42644616+byMax2@users.noreply.github.com>
2020-10-27 14:39:49 +0100

    Updated the game
    Removed the buttons and added icon

---
+++ LemongameReact/precache-manifest.3b62f377ff77ae30ee56134d81f02f49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f040be5b343e48fad8a826dbc043b5de",
+    "url": "/LemongameReact/index.html"
+  },
+  {
+    "revision": "865a7a28a422ae7910c9",
+    "url": "/LemongameReact/static/css/main.1b19bb73.chunk.css"
+  },
+  {
+    "revision": "bc276e908aa7935cfd07",
+    "url": "/LemongameReact/static/js/2.a28b72a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LemongameReact/static/js/2.a28b72a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "865a7a28a422ae7910c9",
+    "url": "/LemongameReact/static/js/main.1425ec90.chunk.js"
+  },
+  {
+    "revision": "c99d978aca42ce260f57",
+    "url": "/LemongameReact/static/js/runtime-main.55898d56.js"
+  }
+]);
