6cd6082b0d7240202353f9e513a00a2269e1cc25
Max <42644616+byMax2@users.noreply.github.com>
2021-01-14 15:58:45 +0100

    Bugs fixed and cost Adjustments

---
+++ LemongameReact/precache-manifest.e176b316e3a74ff47d37ce6e91a2385c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98222887eb1f42a9be4babfd310dbe12",
+    "url": "/LemongameReact/index.html"
+  },
+  {
+    "revision": "9a018872b11d38d784cb",
+    "url": "/LemongameReact/static/css/main.75e611dc.chunk.css"
+  },
+  {
+    "revision": "09b4e639dcbcd037023c",
+    "url": "/LemongameReact/static/js/2.b39eac08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LemongameReact/static/js/2.b39eac08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a018872b11d38d784cb",
+    "url": "/LemongameReact/static/js/main.89c12752.chunk.js"
+  },
+  {
+    "revision": "c99d978aca42ce260f57",
+    "url": "/LemongameReact/static/js/runtime-main.55898d56.js"
+  }
+]);
